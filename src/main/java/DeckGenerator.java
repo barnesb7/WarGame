458f0e46c7fff0bfc55afc57925ac2d98a80baf3
Brian B <brianb@MacBook-Pro-4.local>
2019-02-14 16:37:41 -0500

    Adds Card class w/ properties, setters, getters

---
+++ src/main/java/DeckGenerator.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DeckGenerator {
+
+//    ArrayList<String> cardSuits = new ArrayList<>(Arrays.asList("spades", "hearts", "clubs", "diamonds"));
+//    ArrayList<String> cardRanks = new ArrayList<>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"));
+//
+//
+//    public ArrayList<Card>
+
+}
